3ed35f995be85e960e2678c8c5b470afd3ce2c0d
Hagiwara Tsutomu <tenkao@gmail.com>
2023-05-10 12:26:15 +0900

    1.2.5 コンポーネントの分割
    Slider コンポーネントを追加

---
+++ src/Slider.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let images
+
+  // 画像スライダー表示のための変数・関数
+  let centerIndex = 0
+  let leftIndex = images.length - 1
+  let rightIndex = 1
+
+  function moveLeft() {
+    const length = images.length
+    centerIndex = (centerIndex - 1 + length) % length
+    leftIndex = (centerIndex - 1 + length) % length
+    rightIndex = (centerIndex + 1) % length
+  }
+
+  function moveRight() {
+    const length = images.length
+    centerIndex = (centerIndex + 1) % length
+    leftIndex = (centerIndex - 1 + length) % length
+    rightIndex = (centerIndex + 1) % length
+  }
+</script>
+
+<div class="slider">
+  <img src="{images[leftIndex]}" alt="スライダー画像（左）" class="slider-item left">
+  <img src="{images[centerIndex]}" alt="スライダー画像" class="slider-item">
+  <img src="{images[rightIndex]}" alt="スライダー画像（右）" class="slider-item right">
+  <button class="slider-left-button" on:click={moveLeft}> ← </button>
+  <button class="slider-right-button" on:click={moveRight}> → </button>
+</div>
+
+<style>
+  .slider {
+    position: relative;
+    width: 80%;
+    margin: 0 10%;
+  }
+
+  .slider-item {
+    width: 100%;
+  }
+
+  .slider-item.left {
+    position: absolute;
+    top: 0;
+    right: 100%;
+  }
+
+  .slider-item.right {
+    position: absolute;
+    top: 0;
+    left: 100%;
+  }
+
+  .slider-left-button {
+    position: absolute;
+    top: 50%;
+    right: 100%;
+  }
+
+  .slider-right-button {
+    position: absolute;
+    top: 50%;
+    left: 100%;
+  }
+</style>
